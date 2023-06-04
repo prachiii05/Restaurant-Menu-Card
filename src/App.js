e41ccd224c5f3ab91b0073c8b25b080a0fef8107
Prachi Gupta <107272766+prachiii05@users.noreply.github.com>
2023-06-05 00:20:02 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import Khanaadda from "./component/basics/Khanaadda"
+const App = () => {
+  return <Khanaadda/>;
+};
+
+export default App;
---
+++ src/component/basics/MenuCard.js
@@ -0,0 +1,37 @@
+import React from 'react'
+
+const MenuCard = ({menuData}) => {
+    //console.log(menuData);
+  return (
+    <>
+    <section className='main-card--cointainer'>
+    {
+        menuData.map((curElem) =>{
+            const {id,name,category,image,description}=curElem;
+
+            return(
+<>
+<div className="card-container"key={curElem.id}>
+<div className="card">
+<div className="card-body">
+<span className ="card-Number card-circle subtle">{id}</span>
+<span className='card-author subtle'>{name}</span>
+<h2 className='card-title'> {name} </h2>
+<span className='card-description subtle'>{description}</span>
+<div className='card-read'>Read</div>
+ </div>
+ <img src={curElem.image} alt='images' className='card-media'/>
+ <span className ="card-tag" >Order Now </span>
+</div>
+
+  </div>
+  </>
+            );
+        })
+    }
+</section>
+    </>
+  );
+};
+
+export default MenuCard;
